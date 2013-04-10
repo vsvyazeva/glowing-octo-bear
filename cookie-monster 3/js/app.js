e0b2e6011cb3a2aa44ce16d4ddc69f7041b477a7
Viktoriya Svyazeva <vsvyazeva@yahoo.com>
2013-04-09 21:10:17 -0400

    Initial file

---
+++ cookie-monster 3/js/app.js
@@ -0,0 +1,13 @@
+$(function(event){
+
+    $("#startSwapping").click(function(event){
+    	$("body").css("background-image", "url(\"images/bg1.jpg\")");
+    	$("h2").html("Peanut Butter Cookie Evangelist");
+    	$("h1").html("Ugly Monster");
+
+    });
+
+
+
+
+});
